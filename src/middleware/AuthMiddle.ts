dec16664ea77346713849ae3aa611e384b8ae47e
Igor <ig.vp@hotmail.com>
2022-01-27 02:57:39 -0300

    starting the login route

--- src/middleware/AuthMiddle.ts
+++ src/middleware/AuthMiddle.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { JwtPayload } from "jsonwebtoken";
+import Joi from "joi";
 import LibJwt from "../lib/jtw";
 export default {
   async AuthMiddle(req: Request, res: Response, next: NextFunction) {
@@ -10,6 +10,25 @@ export default {
       next();
     } catch (_e) {
       res.status(400).json({ error: "malformed token" });
+    }
+  },
+  async LoginMiddle(req: Request, res: Response, next: NextFunction) {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    });
+    const options = {
+      abortEarly: false,
+    };
+    const { error } = schema.validate(req.body, options);
+    if (error) {
+      res.status(400).json({
+        error: `Validation error: ${error.details
+          .map((error) => error.message)
+          .join(", ")}`,
+      });
+    } else {
+      next();
     }
   },
 };
